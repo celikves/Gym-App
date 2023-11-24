343264bce0e724c02186f9f3b7dd437681fb638b
Vesile <vesile_emre@hotmail.com>
2023-11-24 10:37:09 +0300

    configurating files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='app'>
+    <div className='app bg-gray-20'>
       App
     </div>
   )
