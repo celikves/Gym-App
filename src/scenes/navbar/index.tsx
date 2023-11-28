3c7c604cc32f3ea437461d6d20d74fbb8e995459
Vesile <vesile_emre@hotmail.com>
2023-11-28 14:22:30 +0300

    selected page updated according to enum , home page updatedaccording to fixed top menu bar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,38 +1,54 @@
 import { useState } from "react"
-import { Bars3Icon,XMarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from '@/assets/Logo.png';
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQueries";
+import ActionButton from "@/shared/ActionButton";
 
-type Props = {}
+type Props = {
+    selectedPage: SelectedPage,
+    setSelectedPage: (value: SelectedPage) => void;
+}
 
-const Navbar = (props: Props) => {
-    const flexBetween = "flex items-center justify-between  "
-  return (
-    <nav>
-        <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-            <div className={`${flexBetween} mx-auto w-5/6 `}>
-                <div className={`${flexBetween} w-full gap-16`}>
-                    {/* LEFT SIDE */}
-                    <img alt="logo" src={Logo}/>
-                    {/* RIGHT SIDE */}
-                    <div className={`${flexBetween} w-full`}>
-                        <div className={`${flexBetween} gap-8 text-sm`}>
-                            <p>Home</p>
-                            <p>Benefits</p>
-                            <p>Our Classes</p>
-                            <p>Contact Us</p>
-                        </div>
-                        <div className={`${flexBetween} gap-8`}>
-                            <p>Sign In</p>
-                            <button>Become a Member</button>
-                        </div>
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+    const flexBetween = "flex items-center justify-between ";
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const [isMenuToggled,setIsMenuToggled] = useState<boolean>(false);
+    return (
+        <nav>
+            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+                <div className={`${flexBetween} mx-auto w-5/6 `}>
+                    <div className={`${flexBetween} w-full gap-16`}>
+                        {/* LEFT SIDE */}
+                        <img alt="logo" src={Logo} />
 
+                        {/* RIGHT SIDE */}
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexBetween} w-full`}>
+                                <div className={`${flexBetween} gap-8 text-sm`}>
+                                    <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                    <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                    <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                    <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                                </div>
+                                <div className={`${flexBetween} gap-8`}>
+                                    <p>Sign In</p>
+                                    <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
+                                </div>
+                            </div>
+                        ) :
+                            <button className="rounded-full bg-secondary-500 p-2" onClick={()=>setIsMenuToggled(!isMenuToggled )}>
+                                <Bars3Icon className="h-6 w-6 text-white" /> 
+                            </button>
+
+                        }
                     </div>
                 </div>
             </div>
-        </div>
-    </nav>
-    
-  )
+        </nav>
+
+    )
 }
 
 export default Navbar
