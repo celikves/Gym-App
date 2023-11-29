56a140b506ef8f65dbcea4e5b693c025f91269f1
Vesile <vesile_emre@hotmail.com>
2023-11-29 14:49:20 +0300

    Home page signature created. navbar background added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import Navbar from '@/scenes/navbar/index.tsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
-
+import Home from '@/scenes/home/index.tsx';
 
 
 function App() {
   const [selectedPage,setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage,setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY===0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.scrollY !==0){
+        setIsTopOfPage(false);
+      }
+    }
+    window.addEventListener("scrol", handleScroll);
+    return () => window.removeEventListener("scroll",handleScroll)
+  },[]);
+
   return (
     <div className='app bg-gray-20'>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}  />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}  />
+      {/* <Home selectedPage={selectedPage}/> */}
     </div>
   )
 }
---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,22 @@
+import useMediaQuery from "@/hooks/useMediaQueries";
+import ActionButton from "@/shared/ActionButton";
+import { SelectedPage } from "@/shared/types";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedbull from "@/assets/SponsorRedbull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+
+
+type Props = {
+    setSelectedpage:(value:SelectedPage) => void ;
+}
+
+const Home = ({setSelectedpage}: Props) => {
+    //const isAboveMediumScreens = 
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
